9c57567f9ddeb36309404400345da7141a64a3e6
emircand <emircandemirel@yahoo.com>
2022-11-10 16:47:31 +0300

    firebase initialization

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,21 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_infinite_list/app.dart';
 import 'package:flutter_infinite_list/simple_bloc_observer.dart';
 
+/*added libs @emircand*/
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+/* changed by emircand
 void main() {
+  Bloc.observer = SimpleBlocObserver();
+  runApp(App());
+}*/
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   Bloc.observer = SimpleBlocObserver();
   runApp(App());
 }
