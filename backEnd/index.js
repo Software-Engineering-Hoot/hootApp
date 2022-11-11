18a327b1a1f96a46fb7d2a0363ea94299deaa92e
dogukan.tastan <dogukan.tastantestinium.com>
2022-11-11 16:06:57 +0300

    Database ilişkilerdirilmesi yapıldı

--- backEnd/index.js
+++ backEnd/index.js
@@ -1,3 +1,25 @@
-console.log("tarik")
 
-let tarik = "arsfda"
+const admin = require('firebase-admin');
+const serviceAccount = require('./service-account-key.json');
+const fs = require('fs')
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+
+const db = admin.firestore();
+
+//firestore post
+function postData() {
+
+    //heroes.json dosyası okunuyor
+    const jsonFile = fs.readFileSync('./heroes.json')
+    const heroes = JSON.parse(jsonFile);
+
+    //dota/heroes içerisine json file'ı bind ediyor.
+    return db.collection('dota').doc('heroes')
+        .set(heroes).then(() => {
+            console.log("Fresh Meat!!");
+        });
+};
+
+postData();
