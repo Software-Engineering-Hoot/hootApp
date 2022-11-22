753c084f59c872a4d5c6adbb4059b53006a5e0ef
emircand <emircandemirel@yahoo.com>
2022-11-22 22:44:18 +0300

    database request&response operations

---
+++ backEnd/README.md
@@ -0,0 +1,7 @@
+#RUN THESE COMMANDS BEFORE FIRST EXECUTION
+npm install --save express
+npm install --save nodemon
+
+##RUN THIS COMMAND TO EXECUTE
+nodemon index.js
+http://localhost:8080 -> GO THIS URL
--- backEnd/index.js
+++ backEnd/index.js
@@ -1,42 +1,44 @@
 const admin = require('firebase-admin');
 const serviceAccount = require('./service-account-key.json');
 const fs = require('fs')
+const express = require('express');
+const app = express();
+const cors = require('cors');
+app.use(cors());
+
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
 });
 
 const db = admin.firestore();
 
-//firestore post
-function postData() {
 
-    //users.json dosyası okunuyor
-    const jsonFile = fs.readFileSync('./users.json')
-    const users = JSON.parse(jsonFile);
+app.get('/', (req, res) => {
+    res.send("Hello from index.js ");
+})
 
-    //userDB/users içerisine json file'ı bind ediyor.
+app.get('/usersfromfirebase', (req, res) => {
+    const docRef = db.doc("userDB/users");
+
+    docRef.get().then((data) => {
+        if (data && data.exists) {
+            const responseData = data.data();
+            res.send(JSON.stringify(responseData, null, "  "));
+        }
+    })
+});
+
+app.post('/userstofirebase', (req, res) => {
+    //firestore post
+    const jsonFile = fs.readFileSync('./users.json') //reads from local
+    const users = JSON.parse(jsonFile); //json parse 
+
     return db.collection('userDB').doc('users')
         .set(users).then(() => {
-            console.log("Fresh Meat!!");
+            res.send("!!")
         });
-};
+})
 
-// GET 
-function getDataFirestore() {
-
-    try {
-
-        //firestore üzerinde userDB/users pathini referans alıyor
-        const docRef = db.doc("userDB/users");
-
-        docRef.get().then((data) => {
-            if (data && data.exists) {
-                const responseData = data.data();
-                console.log(JSON.stringify(responseData, null, "  "));
-            }
-        })
-    } catch (error) {}
-
-}
-
-getDataFirestore();
+app.listen(8080, () => {
+    console.log("listening on port http://localhost:8080")
+})
