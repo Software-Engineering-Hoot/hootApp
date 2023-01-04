181ca4ae62ee66eb2e83c8fae0f507613a2d071c
MSC <sefacahyir@gmail.com>
2023-01-05 01:49:49 +0300

    phone number fix

--- lib/posts/widgets/edit_user_bottom_sheet.dart
+++ lib/posts/widgets/edit_user_bottom_sheet.dart
@@ -16,123 +16,127 @@ Future<void> editUserBottomSheet(
     context: aContext,
     isScrollControlled: true,
     builder: (BuildContext context) {
-      return Container(
-        decoration: const BoxDecoration(
-            borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(20), topRight: Radius.circular(20)),
-            color: colorBox),
-        padding: const EdgeInsets.all(16),
-        child: Form(
-          key: formKey,
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Text(
-                'Update user',
-                style: boldTextStyle(color: textPrimaryColor),
-              ),
-              const Divider().paddingOnly(top: 10, bottom: 10),
-              AppTextField(
-                textFieldType: TextFieldType.NAME,
-                inputFormatters: <TextInputFormatter>[
-                  FilteringTextInputFormatter.allow(RegExp("[0-9a-zA-Z]"))
-                ],
-                decoration: const InputDecoration(
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(18))),
-                    labelText: 'Name'),
-                initialValue: user.name,
-                onChanged: (value) {
-                  user.name = value;
-                },
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter some name';
-                  }
-                  return null;
-                },
-              ),
-              16.height,
-              AppTextField(
-                textFieldType: TextFieldType.NAME,
-                inputFormatters: <TextInputFormatter>[
-                  FilteringTextInputFormatter.allow(RegExp("[0-9a-zA-Z]"))
-                ],
-                decoration: const InputDecoration(
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(18))),
-                    labelText: 'Surname'),
-                initialValue: user.surname,
-                onChanged: (value) {
-                  user.surname = value;
-                },
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter surname';
-                  }
-                  return null;
-                },
-              ),
-              16.height,
-              AppTextField(
-                textFieldType: TextFieldType.EMAIL,
-                enabled: false,
-                decoration: const InputDecoration(
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(18))),
-                    labelText: 'Email'),
-                initialValue: user.email,
-                onChanged: (value) {
-                  user.email = value;
-                },
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter email address';
-                  }
-                  return null;
-                },
-              ),
-              16.height,
-              AppTextField(
-                maxLength: 14,
-                textFieldType: TextFieldType.NUMBER,
-                inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-                decoration: const InputDecoration(
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(18))),
-                    labelText: 'Phone Number'),
-                initialValue: user.phoneNumber,
-                onChanged: (value) {
-                  user.phoneNumber = value;
-                },
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter phone number';
-                  }
-                  return null;
-                },
-              ),
-              16.height,
-              AppButton(
-                color: colorPrimary,
-                width: context.width(),
-                child: Text('Update User',
-                    style: boldTextStyle(color: Colors.white)),
-                onTap: () {
-                  if (formKey.currentState!.validate()) {
-                    formKey.currentState!.save();
-                    _advertService
-                        .editUser(user)
-                        .then((value) => Navigator.pop(context));
-                  }
-                },
-              ).cornerRadiusWithClipRRect(30).paddingOnly(
-                    left: context.width() * 0.1,
-                    right: context.width() * 0.1,
-                    bottom: 10,
-                  )
-            ],
+      return Padding(
+        padding:
+            EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
+        child: Container(
+          decoration: const BoxDecoration(
+              borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20), topRight: Radius.circular(20)),
+              color: colorBox),
+          padding: const EdgeInsets.all(16),
+          child: Form(
+            key: formKey,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  'Update user',
+                  style: boldTextStyle(color: textPrimaryColor),
+                ),
+                const Divider().paddingOnly(top: 10, bottom: 10),
+                AppTextField(
+                  textFieldType: TextFieldType.NAME,
+                  inputFormatters: <TextInputFormatter>[
+                    FilteringTextInputFormatter.allow(RegExp("[0-9a-zA-Z]"))
+                  ],
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(18))),
+                      labelText: 'Name'),
+                  initialValue: user.name,
+                  onChanged: (value) {
+                    user.name = value;
+                  },
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter some name';
+                    }
+                    return null;
+                  },
+                ),
+                16.height,
+                AppTextField(
+                  textFieldType: TextFieldType.NAME,
+                  inputFormatters: <TextInputFormatter>[
+                    FilteringTextInputFormatter.allow(RegExp("[0-9a-zA-Z]"))
+                  ],
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(18))),
+                      labelText: 'Surname'),
+                  initialValue: user.surname,
+                  onChanged: (value) {
+                    user.surname = value;
+                  },
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter surname';
+                    }
+                    return null;
+                  },
+                ),
+                16.height,
+                AppTextField(
+                  textFieldType: TextFieldType.EMAIL,
+                  enabled: false,
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(18))),
+                      labelText: 'Email'),
+                  initialValue: user.email,
+                  onChanged: (value) {
+                    user.email = value;
+                  },
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter email address';
+                    }
+                    return null;
+                  },
+                ),
+                16.height,
+                AppTextField(
+                  maxLength: 14,
+                  textFieldType: TextFieldType.NUMBER,
+                  inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(18))),
+                      labelText: 'Phone Number'),
+                  initialValue: user.phoneNumber,
+                  onChanged: (value) {
+                    user.phoneNumber = value;
+                  },
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter phone number';
+                    }
+                    return null;
+                  },
+                ),
+                16.height,
+                AppButton(
+                  color: colorPrimary,
+                  width: context.width(),
+                  child: Text('Update User',
+                      style: boldTextStyle(color: Colors.white)),
+                  onTap: () {
+                    if (formKey.currentState!.validate()) {
+                      formKey.currentState!.save();
+                      _advertService
+                          .editUser(user)
+                          .then((value) => Navigator.pop(context));
+                    }
+                  },
+                ).cornerRadiusWithClipRRect(30).paddingOnly(
+                      left: context.width() * 0.1,
+                      right: context.width() * 0.1,
+                      bottom: 10,
+                    )
+              ],
+            ),
           ),
         ),
       );
